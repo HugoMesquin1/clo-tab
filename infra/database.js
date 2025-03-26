7da6607af39ba5d981bfc931f116525c274c177c
Hugo Vinicius Mesquini <101219708+HugoMesquin1@users.noreply.github.com>
2025-03-26 18:25:27 +0000

    adds eslint job in linting github

--- infra/database.js
+++ infra/database.js
@@ -8,6 +8,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.error(error);
   } finally {
     await client.end();
   }
