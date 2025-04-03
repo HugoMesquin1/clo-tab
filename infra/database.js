78b1afa416f948b2580d45b2c5ddf6edfc668229
Hugo Vinicius Mesquini <101219708+HugoMesquin1@users.noreply.github.com>
2025-04-03 13:21:53 +0000

    refactor: refactor errors.js/database.js/tests

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,22 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
-
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro no código do database");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
 }
-
 async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -25,19 +26,19 @@ async function getNewClient() {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
-
   await client.connect();
   return client;
 }
-const db = { query, getNewClient };
-export default db;
-
+const database = {
+  query,
+  getNewClient,
+};
+export default database;
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
     };
   }
-
   return process.env.NODE_ENV === "production" ? true : false;
 }
